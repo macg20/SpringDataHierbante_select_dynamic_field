29ef51d862aa070fcc159e5e698955e542a0e9dc
emgie <macgryczka20@gmail.com>
2022-03-30 18:30:57 +0200

    initial application

---
+++ src/test/java/com/github/macg20/dynamicselectionfield/DynamicSelectionFieldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.macg20.dynamicselectionfield;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamicSelectionFieldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
